63d35b024898faf7ac443c17c38de4b52613a652
Matej <Matej@KOMP>
2013-08-11 23:22:55 +0200

    Fixed #96

--- src/main/java/com/mcnsa/flatcore/checkpoints/usercommands/SaveCommand.java
+++ src/main/java/com/mcnsa/flatcore/checkpoints/usercommands/SaveCommand.java
@@ -75,7 +75,7 @@ public class SaveCommand extends BaseCheckpointUserCommand {
 		lastCheckpoint = new SavedCheckpoint();
 		lastCheckpoint.list = checkpointList;
 		lastCheckpoint.position = position;
-		lastCheckpoint.location = Util.unserializeLocation(stringList.get(position - 1));
+		lastCheckpoint.location = Util.unserializeLocation(stringList.get(position));
 		
 		CheckpointsModule.savedCheckpoints.put(player.getName(), lastCheckpoint);
 		
