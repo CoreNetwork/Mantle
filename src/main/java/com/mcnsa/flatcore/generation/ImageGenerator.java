df7e78abebab12feafae3c66949df38fcc793f2a
Matej <Matej@KOMP>
2013-08-14 11:48:50 +0200

    Removed debug spam

--- src/main/java/com/mcnsa/flatcore/generation/ImageGenerator.java
+++ src/main/java/com/mcnsa/flatcore/generation/ImageGenerator.java
@@ -4,8 +4,6 @@ import net.minecraft.server.v1_6_R2.Block;
 import net.minecraft.server.v1_6_R2.Material;
 import net.minecraft.server.v1_6_R2.MaterialMapColor;
 
-import com.mcnsa.flatcore.FCLog;
-
 public class ImageGenerator {
 	public static void generate()
 	{
@@ -17,9 +15,6 @@ public class ImageGenerator {
 		Block block = Block.byId[id];
 		Material material = block.material;
 		MaterialMapColor color = material.H;
-
-		FCLog.info(Integer.toString(color.p));
-		FCLog.info(Integer.toHexString(color.p));
 		
 		return color.p;
 
