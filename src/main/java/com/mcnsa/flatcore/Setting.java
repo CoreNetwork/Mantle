637b732585a182c9d51161ec0996eee702cf096d
Matej <Matej@KOMP>
2013-08-16 01:03:58 +0200

    Cleaning up config.yml

--- src/main/java/com/mcnsa/flatcore/Setting.java
+++ src/main/java/com/mcnsa/flatcore/Setting.java
@@ -2,57 +2,11 @@ package com.mcnsa.flatcore;
 
 import java.util.Arrays;
 
-public enum Setting {
-	MAP_MIN_X("Map.MinX", -10000),
-	MAP_MIN_Z("Map.MinZ", -10000),
-	MAP_MAX_X("Map.MaxX", 10000),
-	MAP_MAX_Z("Map.MaxZ", 10000),
-	GENERATION_MIN_X("Generation.MinX", -10000),
-	GENERATION_MIN_Z("Generation.MinZ", -10000),
-	GENERATION_MAX_X("Generation.MaxX", 10000),
-	GENERATION_MAX_Z("Generation.MaxZ", 10000),
-	MAP_PORTAL_MIN_Y("Map.PortalMinY", 10),
-	MAP_PORTAL_MAX_Y("Map.PortalMaxY", 100),
-	MAP_MOVE_PORTALS_WITH_LOWER_Y("Map.MovePortalsWithLowerY", 20),
-	MAP_MOVE_PORTALS_WITH_HIGHER_Y("Map.MovePortalsWithHigherY", 300),
-	NETHER_MAX_X("Map.NetherMaxX", 625),
-	NETHER_MIN_X("Map.NetherMinX", -625),
-	NETHER_MAX_Z("Map.NetherMaxZ", 625),
-	NETHER_MIN_Z("Map.NetherMinZ", -625),
-	NETHER_PORTAL_MIN_Y("Nether.PortalMinY", 10),
-	NETHER_MOVE_PORTALS_WITH_LOWER_Y("Nether.MovePortalsWithLowerY", 20),
-	NETHER_MOVE_PORTALS_WITH_HIGHER_Y("Nether.MovePortalsWithHigherY", 120),
-	NETHER_PORTAL_MAX_Y("Nether.PortalMaxY", 100),
-	
+public enum Setting {	
 	TELEPORT_Y("Respawn.TeleportY", 14),
 	SPAWN_PROTECTION_LENGTH("Respawn.SpawnProtectionLength", 120),
 	SPAWN_PROTECTION_NOTIFICATIONS("Respawn.SpawnProtectionNotifications", Arrays.asList(new Integer[] {90, 60, 30, 10})),
 	MOB_REMOVAL_RADIUS_SQUARED("Respawn.MobRemovalRadiusSquared", 625),
-
-	VILLAGE_GRID_SPACE("Village.Spacing", 500),
-	VILLAGE_RANDOM_OFFSET("Village.RandomOffset", 150),
-	NUMBER_OF_VILLAGES("Village.NumberOfSchematics", 6),
-	VILLAGE_PASTING_Y("Village.PastingY", 1),
-	VILLAGE_OFFSET_X("Village.OffsetX", 0),
-	VILLAGE_OFFSET_Z("Village.OffsetZ", 0),
-	
-	CAMPFIRE_GRID_SPACE("Campfire.Spacing", 500),
-	CAMPFIRE_PASTING_Y("Campfire.PastingY", 1),
-	CAMPFIRE_RANDOM_OFFSET("Campfire.RandomOffset", 150),
-	CAMPFIRE_OFFSET_X("Campfire.OffsetX", 250),
-	CAMPFIRE_OFFSET_Z("Campfire.OffsetZ", 250),
-	NUMBER_OF_NORMAL_CAMPFIRES("Campfire.NumberOfNormalSchematics", 2),
-	NUMBER_OF_TRAPPED_CAMPFIRES("Campfire.NumberOfTrappedSchematics", 2),
-	CAMPFIRE_TRAP_CHANCE("Campfire.TrapChance", 10),
-	CAMPFIRE_PROTECTION_RADIUS("Campfire.ProtectionRadius", 40),
-	
-	OUTPOST_GRID_SPACE("Outpost.Spacing", 500),
-	OUTPOST_PASTING_Y("Outpost.PastingY", 1),
-	OUTPOST_RANDOM_OFFSET("Outpost.RandomOffset", 150),
-	OUTPOST_OFFSET_X("Outpost.OffsetX", 350),
-	OUTPOST_OFFSET_Z("Outpost.OffsetZ", 350),
-	NUMBER_OF_OUTPOSTS("Outpost.NumberOfSchematics", 2),
-	OUTPOST_PROTECITON_RADIUS("Outpost.ProtectionRadius", 50),
 	
 	RESORATION_VILLAGE_CHECK_PADDING("Restoration.VillageCheckPadding", 10),
 	RESTORATION_VILLAGE_CHECK_PERIOD("Restoration.VillageCheckPeriod", 1800),
