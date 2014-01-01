7d58c58145cd23ccb213779a1820927504d4d688
Matej <matejdro@gmail.com>
2014-01-01 21:18:03 +0100

    Exploding smoke effect

--- src/main/java/us/corenetwork/mantle/slimespawning/SlimeKillTimer.java
+++ src/main/java/us/corenetwork/mantle/slimespawning/SlimeKillTimer.java
@@ -4,8 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Effect;
 import org.bukkit.FireworkEffect;
+import org.bukkit.Sound;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Slime;
+
+import us.corenetwork.mantle.ParticleLibrary;
 
 public class SlimeKillTimer implements Runnable {
     public static SlimeKillTimer timerSingleton;
@@ -28,16 +33,16 @@ public class SlimeKillTimer implements Runnable {
         	
         	for (Slime slime : overworld.getEntitiesByClass(Slime.class))
         	{
-//        		for (Entity e : slime.getNearbyEntities(10, 10, 10))
-//        		{
-//        			if (e instanceof Player)
-//        			{
-//                		ParticleLibrary.s.sendToPlayer((Player) e, slime.getLocation(), 0, 0, 0, 0,  10);
-//        			}
-//        		}
-//        		
-//        		slime.getWorld().playSound(slime.getLocation(), Sound.ENDERMAN_DEATH, 1f, 1f);
-//        		
+        		for (Entity e : slime.getNearbyEntities(10, 10, 10))
+        		{
+        			if (e instanceof Player)
+        			{
+                		ParticleLibrary.EXPLODE.sendToPlayer((Player) e, slime.getLocation(), 0, 0, 0, 0,  10);
+        			}
+        		}
+        		
+        		slime.getWorld().playSound(slime.getLocation(), Sound.SLIME_WALK, 1f, 1f);
+        		
         		slime.getWorld().playEffect(slime.getLocation(), Effect.SMOKE, 0);
         		slime.remove();
         	}
