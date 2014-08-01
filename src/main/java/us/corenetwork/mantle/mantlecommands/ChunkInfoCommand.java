e724b3009edb3e3640f670c4ad2426f921a7f9d3
Matej <matejdro@gmail.com>
2014-08-01 12:55:29 +0200

    Chunk commands now don't need players, new gitignore

--- src/main/java/us/corenetwork/mantle/mantlecommands/ChunkInfoCommand.java
+++ src/main/java/us/corenetwork/mantle/mantlecommands/ChunkInfoCommand.java
@@ -1,25 +1,12 @@
 package us.corenetwork.mantle.mantlecommands;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.List;
-
-import net.minecraft.server.v1_7_R3.EntityPlayer;
-import net.minecraft.server.v1_7_R3.PlayerChunkMap;
-import net.minecraft.server.v1_7_R3.WorldServer;
-
-import org.bukkit.Chunk;
-import org.bukkit.World;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
-import org.bukkit.entity.Player;
-
 import us.corenetwork.mantle.MLog;
-import us.corenetwork.mantle.MantlePlugin;
 import us.corenetwork.mantle.Util;
+
+import java.lang.reflect.Field;
 
 
 public class ChunkInfoCommand extends BaseMantleCommand {	
@@ -27,17 +14,27 @@ public class ChunkInfoCommand extends BaseMantleCommand {
 	{
 		permission = "chunkinfo";
 		desc = "Display chunk info for current world";
-		needPlayer = true;
+		needPlayer = false;
 	}
 
 
 	public void run(final CommandSender sender, String[] args) {
-		Player player = (Player) sender;
+        if (args.length < 1)
+        {
+            Util.Message("Usage: /mantle chunkinfo [world]", sender);
+            return;
+        }
+
+        CraftWorld world = (CraftWorld) Bukkit.getWorld(args[0]);
+        if (world == null)
+        {
+            Util.Message("Invalid world!", sender);
+            return;
+        }
 
 		try
 		{
 
-			World world = player.getWorld();
 			int loadedChunks = world.getLoadedChunks().length;
 			int chunkGCTickCount = 0;  
 
--- src/main/java/us/corenetwork/mantle/mantlecommands/DumpChunksCommand.java
+++ src/main/java/us/corenetwork/mantle/mantlecommands/DumpChunksCommand.java
@@ -1,5 +1,15 @@
 package us.corenetwork.mantle.mantlecommands;
 
+import net.minecraft.server.v1_7_R3.EntityPlayer;
+import net.minecraft.server.v1_7_R3.PlayerChunkMap;
+import net.minecraft.server.v1_7_R3.WorldServer;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
+import us.corenetwork.mantle.MantlePlugin;
+import us.corenetwork.mantle.Util;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -7,36 +17,35 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 
-import net.minecraft.server.v1_7_R3.EntityPlayer;
-import net.minecraft.server.v1_7_R3.PlayerChunkMap;
-import net.minecraft.server.v1_7_R3.WorldServer;
-
-import org.bukkit.Chunk;
-import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
-import org.bukkit.entity.Player;
-
-import us.corenetwork.mantle.MantlePlugin;
-import us.corenetwork.mantle.Util;
-
 
 public class DumpChunksCommand extends BaseMantleCommand {	
 	public DumpChunksCommand()
 	{
 		permission = "dumpchunks";
 		desc = "Dump chunk data";
-		needPlayer = true;
+		needPlayer = false;
 	}
 
 
 	public void run(final CommandSender sender, String[] args) {
+        if (args.length < 1)
+        {
+            Util.Message("Usage: /mantle dumpchunks [world]", sender);
+            return;
+        }
+
+        CraftWorld world = (CraftWorld) Bukkit.getWorld(args[0]);
+        if (world == null)
+        {
+            Util.Message("Invalid world!", sender);
+            return;
+        }
+
 		Util.Message("Dumping chunk debug data...", sender);
 
 		File file = new File(MantlePlugin.instance.getDataFolder(), "chunkdata.txt");
 
-		Player player = (Player) sender;
 
-		CraftWorld world = (CraftWorld) player.getWorld();
 		WorldServer nmsWorld = world.getHandle();
 		PlayerChunkMap map = nmsWorld.getPlayerChunkMap();
 
