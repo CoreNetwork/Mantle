f5933b468718d2d82ad5c9fe73c026549a83fb08
eliadil <ginafff@gmail.com>
2015-03-05 16:00:58 +0100

    Fixed full chest of nuggets not being fused to block form properly

--- src/main/java/us/corenetwork/mantle/spellbooks/books/FusingBook.java
+++ src/main/java/us/corenetwork/mantle/spellbooks/books/FusingBook.java
@@ -136,7 +136,7 @@ public class FusingBook extends Spellbook {
 
 			}
 
-			freeInventorySlots -= stacksAdded;
+			freeInventorySlots = freeInventorySlots - stacksAdded + stacksRemoved;
 		}
 		
 		if (inventory.getType() == InventoryType.PLAYER)
