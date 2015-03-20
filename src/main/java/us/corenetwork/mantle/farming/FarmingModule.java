0071d70ea002de77bfbcd74713835d2c480f8144
eliadil <ginafff@gmail.com>
2015-03-20 05:11:50 +0100

    #370 Netherwart custom description

--- src/main/java/us/corenetwork/mantle/farming/FarmingModule.java
+++ src/main/java/us/corenetwork/mantle/farming/FarmingModule.java
@@ -1,15 +1,17 @@
 package us.corenetwork.mantle.farming;
 
+import com.comphenix.protocol.ProtocolLibrary;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import us.corenetwork.mantle.MantleModule;
 import us.corenetwork.mantle.MantlePlugin;
+import us.corenetwork.mantle.restockablechests.CompassProtocolListener;
 
 public class FarmingModule extends MantleModule {
     public static FarmingModule instance;
     FishingConfig fishingConfig = new FishingConfig();
-
+    private NetherwartProtocolListener netherwartProtocolListener;
     public FarmingModule() {
         super("Farming", null, "farming");
         instance = this;
@@ -21,6 +23,9 @@ public class FarmingModule extends MantleModule {
         Bukkit.getPluginManager().registerEvents(new NetherWartFarming(), MantlePlugin.instance);
         Bukkit.getPluginManager().registerEvents(fishingConfig, MantlePlugin.instance);
 
+        netherwartProtocolListener = new NetherwartProtocolListener();
+        ProtocolLibrary.getProtocolManager().addPacketListener(netherwartProtocolListener);
+
         return true;
     }
 
@@ -28,6 +33,9 @@ public class FarmingModule extends MantleModule {
     public void loadConfig() {
         super.loadConfig();
         fishingConfig.loadConfig();
+
+        if (netherwartProtocolListener != null)
+            netherwartProtocolListener.loadConfig();
     }
 
     @Override
